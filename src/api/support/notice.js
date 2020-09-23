3940ab40ff717b0435bac98570b9bedbcce515c8
박철현 <parkcheolhyun@cheolmaegi.local>
2020-09-23 23:54:31 +0900

    # 서버 업로드

--- src/api/support/notice.js
+++ src/api/support/notice.js
@@ -10,16 +10,36 @@ const list = [
     { id: 6, title: 'TEST 공지사항입니다.', created_at: new Date('2020-06-10'), content: '안녕하세요 아주나무입니다. 더욱 나누며 더욱 겸손히 믿을 수 있는 원재료로 건강하고 맛있게 찾아봡겠습니다 ^^' },
 ];
 
-export const requestNoticeList = async (token) => {
+export const requestNoticeList = async (token, offset, limit) => {
     const req = Paths.api + 'user/notice/list';
     const config = {
         headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${token}`,
         },
+        params: {
+            offset, limit
+        }
     };
     const result = await axios.get(req, config);
     console.log(result);
     // return result.data.query;
     return { notices: list };
 };
+
+export const requestNoticeItem = async (token, id) => {
+    const req = Paths.api + 'user/notice/show';
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+        },
+        params: {
+            id
+        }
+    };
+
+    const result = await axios.get(req, config);
+    console.log(result);
+    return { notice: list.find(item => item.id === id) }
+}
--- src/components/support/Notice.js
+++ src/components/support/Notice.js
@@ -2,50 +2,107 @@ import React, { useCallback, useEffect, useState } from 'react';
 import styles from './Notice.module.scss';
 
 import Message from '../message/Message';
-import { requestNoticeList } from '../../api/support/notice';
-import { useHistory } from 'react-router-dom';
+import { requestNoticeItem, requestNoticeList } from '../../api/support/notice';
+import { Link, useHistory } from 'react-router-dom';
 import { Paths } from '../../paths';
 import { dateToYYYYMMDD } from '../../lib/formatter';
 
-export default () => {
-    const [list, setList] = useState([]);
+
+const numbering = (number) => {
+    let result = number;
+    if (number < 100) {
+        result = "0" + result;
+    } if (number < 10) {
+        result = "0" + result;
+    }
+    return result;
+}
+
+export default ({ match }) => {
+    const [noticeList, setNoticeList] = useState([]);
+    const [noticeItem, setNoticeItem] = useState({});
     const history = useHistory();
+
+    const { params } = match;
 
     const getNoticeList = useCallback(async () => {
         const token = sessionStorage.getItem('access_token');
         try {
             const res = await requestNoticeList(token);
-            setList(res.notices);
+            setNoticeList(res.notices);
         } catch (e) {
             alert('잘못된 접근입니다.');
             history.replace(Paths.index);
         }
     }, [history]);
 
+    const getNoticeItem = useCallback(async () => {
+        const token = sessionStorage.getItem('access_token');
+        try {
+            const res = await requestNoticeItem(token, parseInt(params.id));
+            if (res.notice !== null && res.notice !== undefined) {
+                setNoticeItem(res.notice);
+            }
+        } catch (e) {
+            alert('잘못된 접근입니다.');
+            history.replace(Paths.index);
+        }
+    }, [history, params]);
+
     useEffect(() => {
         getNoticeList();
-    }, [getNoticeList]); 
+    }, [getNoticeList]);
+
+    useEffect(() => {
+        if (params.id !== null && params.id !== undefined) {
+            getNoticeItem();
+        }
+    }, [getNoticeItem, params]);
 
     return (
         <div className={styles['box']}>
             <div className={styles['table']}>
-                {list.length > 0 ?
-                list.map(item => (
-                    <div key={item.id} className={styles['column']}>
-                        <div className={styles['row']}>
-                            <div className={styles['id']}>
-                                {item.id}
-                            </div>
-                            <div className={styles['title']}>
-                                {item.title}
-                            </div>
-                            <div className={styles['created']}>
-                                {dateToYYYYMMDD(item.created_at)}
-                            </div>
-                        </div>
-                    </div>)
-                ) : <Message src={false} msg={"등록된 공지사항이 없습니다."} size={260} />}
+                { params.id !== null && params.id !== undefined ? 
+                <NoticeContent item={noticeItem} />
+                : noticeList.length > 0 ?
+                <NoticeList list={noticeList} />
+                : <Message src={false} msg={"등록된 공지사항이 없습니다."} size={260} />}
             </div>
         </div>
     );
 };
+
+const NoticeList = ({ list }) => list.map(item => (
+    <div key={item.id} className={styles['column']}>
+        <Link to={`${Paths.ajoonamu.support}/notice/${item.id}`} className={styles['row']}>
+            <div className={styles['id']}>
+                {numbering(item.id)}
+            </div>
+            <div className={styles['title']}>
+                {item.title}
+            </div>
+            <div className={styles['created']}>
+                {dateToYYYYMMDD(item.created_at)}
+            </div>
+        </Link>
+    </div>
+));
+
+const NoticeContent = ({ item }) => (
+    <>
+        <div className={styles['head']}>
+            <div className={styles['id']}>
+                {numbering(item.id)}
+            </div>
+            <div className={styles['title']}>
+                {item.title}
+            </div>
+            <div className={styles['created']}>
+                {dateToYYYYMMDD(item.created_at)}
+            </div>
+        </div>
+        <div className={styles['content']}>
+            {item.content}
+        </div>
+    </>
+);
