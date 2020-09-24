0aa0c0c779c126d8794d703909fa7568720d1811
박철현 <parkcheolhyun@cheolmaegi.local>
2020-09-24 10:39:48 +0900

    견적서 보내기.

--- src/components/modal/EstmModal.js
+++ src/components/modal/EstmModal.js
@@ -7,6 +7,9 @@ import { ButtonBase } from '@material-ui/core';
 import { requestPostEstimate } from '../../api/order/estimate';
 import CloseIcon from '../svg/modal/CloseIcon';
 import Estimate from '../assets/Estimate';
+import { useDispatch } from 'react-redux';
+import { modalOpen } from '../../store/modal';
+import Loading from '../assets/Loading';
 
 const reducer = (state, action) => ({
     ...state,
@@ -14,25 +17,47 @@ const reducer = (state, action) => ({
 });
 
 const EstmModal = (props) => {
+    const modalDispatch = useDispatch();
+    const openAlert = useCallback((title, text, handleClick = () => {}) => {
+        modalDispatch(modalOpen(false, title, text, handleClick));
+    }, [modalDispatch]);
+
+    const [loading, setLoading] = useState(false);
     const [fullWidth] = React.useState(true);
     const [maxWidth] = React.useState('sm');
 
     const [state, dispatch] = useReducer(reducer, {
-        receiver: '박철현',
-        receiver_email: 'ghooz1204@naver.com',
+        receiver: '',
+        receiver_email: '',
     });
     const [estmFile, setEstmFile] = useState(null);
 
     const onStateChange = useCallback((e) => dispatch(e.target), []);
     const sendEstimate = useCallback(async () => {
-        const token = sessionStorage.getItem('access_token');
-        const { receiver, receiver_email } = state;
-        requestPostEstimate(token, {
-            estm_email: receiver_email,
-            estm_username: receiver,
-            estm_file: estmFile,
-        });
-    }, [estmFile, state]);
+        if (estmFile) {
+            const token = sessionStorage.getItem('access_token');
+            setLoading(true);
+            try {
+                const { receiver, receiver_email } = state;
+                const res = await requestPostEstimate(token, {
+                    estm_email: receiver_email,
+                    estm_username: receiver,
+                    estm_file: estmFile,
+                });
+                console.log(res);
+                if (res.data.msg === "성공") {
+                    openAlert('성공적으로 전송되었습니다!', '이메일을 확인해 주세요!');    
+                } else {
+                    openAlert('전송이 실패했습니다.', '다시 시도해 주세요.');
+                }
+            } catch (e) {
+                openAlert('예기치 못한 에러가 발생했습니다!', '다시 시도해 주세요.');
+            }
+            setLoading(false);
+        } else {
+            openAlert('미리보기 시도 후 전송하셔야 합니다.', '견적서를 한 번 확인 후에 시도해주세요.');
+        }
+    }, [estmFile, state, openAlert]);
 
     const onDownload = (ref) => {
         let position = 0;
@@ -47,7 +72,6 @@ const EstmModal = (props) => {
 
             doc.addImage(imageData, 'PNG', 0, position, imgWidth, imgHeight);
             heightLeft -= pageHeight;
-
             while (heightLeft >= 20) {
                 position = heightLeft - imgHeight;
                 doc.addPage();
@@ -65,44 +89,48 @@ const EstmModal = (props) => {
             const makeFile = new File([blob], '아주나무 견적서.pdf', {
                 type: blob.type,
             });
+            window.open(doc.output('bloburl'));
             setEstmFile(makeFile);
         });
     };
 
     return (
-        <Dialog
-            fullWidth={fullWidth}
-            maxWidth={maxWidth}
-            open={props.open}
-            onClose={props.handleClose}
-            aria-labelledby="form-dialog-title"
-        >
-            <div className={styles['title-bar']}>
-                <div className={styles['title']}>견적서 발송</div>
-                <div className={styles['close']} onClick={props.handleClose}>
-                    <CloseIcon />
+        <>
+            <Dialog
+                fullWidth={fullWidth}
+                maxWidth={maxWidth}
+                open={props.open}
+                onClose={props.handleClose}
+                aria-labelledby="form-dialog-title"
+            >
+                <div className={styles['title-bar']}>
+                    <div className={styles['title']}>견적서 발송</div>
+                    <div className={styles['close']} onClick={props.handleClose}>
+                        <CloseIcon />
+                    </div>
                 </div>
-            </div>
-            <div className={styles['modal-content']}>
-                <div className={styles['modal-input-box']}>
-                    <div className={styles['label']}>수신자</div>
-                    <input />
-                </div>
-                <div className={styles['modal-input-box']}>
-                    <div className={styles['label']}>받을 이메일 주소</div>
-                    <input />
-                </div>
-                <ButtonBase className={styles['estimate']}>
-                    <Estimate onDownload={onDownload} />
-                </ButtonBase>
-                <div className={styles['box']}>
-                    <ButtonBase className={styles['btn']} onClick={props.order}>
-                        건너뛰기
+                <div className={styles['modal-content']}>
+                    <div className={styles['modal-input-box']}>
+                        <div className={styles['label']}>수신자</div>
+                        <input type="text" name="receiver" value={state.receiver} onChange={onStateChange}/>
+                    </div>
+                    <div className={styles['modal-input-box']}>
+                        <div className={styles['label']}>받을 이메일 주소</div>
+                        <input type="text" name="receiver_email" value={state.receiver_email} onChange={onStateChange}/>
+                    </div>
+                    <ButtonBase className={styles['estimate']}>
+                        <Estimate onDownload={onDownload} />
                     </ButtonBase>
-                    <ButtonBase className={styles['btn'] + ' ' + styles['active']} onClick={sendEstimate}>견적서발송</ButtonBase>
+                    <div className={styles['box']}>
+                        <ButtonBase className={styles['btn']} onClick={props.order}>
+                            건너뛰기
+                        </ButtonBase>
+                        <ButtonBase className={styles['btn'] + ' ' + styles['active']} onClick={sendEstimate}>견적서발송</ButtonBase>
+                    </div>
                 </div>
-            </div>
-        </Dialog>
+            </Dialog>
+            <Loading open={loading} />
+        </>
     );
 };
 
