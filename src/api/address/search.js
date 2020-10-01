ea8f84772513949023b67ed5495c6d2494e0a84d
박창준 <ckdwns9121@naver.com>
2020-10-01 17:36:24 +0900

    # 주소 수정

--- src/api/address/search.js
+++ src/api/address/search.js
@@ -4,7 +4,25 @@ const URL = "http://www.juso.go.kr/addrlink/addrLinkApi.do";
 const KEY= 'devU01TX0FVVEgyMDIwMDgyMzIxNTAzMDExMDA4OTU=';
 
 export const addr_test= async () =>{
-    const query = `${URL}?confmKey=${KEY}&currentPage=1&countPerPage=10&keyword=${"동아대학교"}&resultType=json`;
-    const res = await axios.get(query);
+    const query = `${URL}`;
+    const test = `${URL}?confmKey=${KEY}&currentPage=1&countPerPage=10&keyword=${'동아대학교'}&resultType=json`;
+    
+
+    axios.defaults.headers.common['Access-Control-Allow-Origin'] = `*`;
+
+    axios.defaults.url = test;
+
+    // const config = {
+    //     params: {
+    //         confmKey:KEY,
+    //         currentPage:1,
+    //         countPerPage:10,
+    //         keyword:'동아대학교',
+    //         resultType:'json'
+    //     }
+    // }
+    
+    const res = await axios.get();
+    console.log(res);
     return res.data.results.juso;
 }
